2a1cf63b9911fef8965a34d24212d47b1a9b17bf
Nabil Tamim Abdullah <nabiltamim666@gmail.com>
2024-03-06 23:21:13 +0900

    first commit

--- agung.py
+++ agung.py
@@ -1,10 +1,10 @@
 nama = input("Masukkan nama anda: ")
 hari_kerja = int(input("Masukkan jumlah hari kerja: "))
-hari_kerja_perbulan = 26
-gaji = 6000000
+hari_kerja_perbulan = 25
+gaji = 100000
 
 if hari_kerja > hari_kerja_perbulan:
-    lembur = (hari_kerja - hari_kerja_perbulan) * 200000
+    lembur = (hari_kerja - hari_kerja_perbulan) * 2000
 else:
     lembur = 0
 
