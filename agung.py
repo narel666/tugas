e15931ead95944e773716a67a6d0061a0e7b53e2
narel666 <127743300+narel666@users.noreply.github.com>
2024-03-07 01:17:01 +0900

    agung.py

--- agung.py
+++ agung.py
@@ -1,18 +1,17 @@
-nama = input("Masukkan nama anda: ")
+nama_pekerja = input("Masukkan nama anda: ")
 hari_kerja = int(input("Masukkan jumlah hari kerja: "))
 hari_kerja_perbulan = 25
-gaji = 100000
+gaji = 6000000
 
 if hari_kerja > hari_kerja_perbulan:
-    lembur = (hari_kerja - hari_kerja_perbulan) * 2000
+    lembur = (hari_kerja - hari_kerja_perbulan) * 100000
 else:
     lembur = 0
 
 total_gaji = (hari_kerja / hari_kerja_perbulan) * gaji
 total_gaji += lembur
 
-# Menggunakan string formatting untuk menambahkan titik sebagai pemisah ribuan
 total_gaji_formatted = "{:,.2f}".format(total_gaji)
 
-print("\nNama:", nama)
-print("Total gaji yang diterima adalah: Rp.", total_gaji_formatted)
+print("\nNama:", nama_pekerja)
+print("Upah anda adalah: Rp.", total_gaji_formatted)
